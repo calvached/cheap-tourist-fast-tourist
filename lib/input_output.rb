8ece9786ed9cee979380cd3147eb6f4e2881038e
calvached <calvache.diana@gmail.com>
2014-07-21 11:54:33 -0500

    Adding IO and MockIO in order to have different return outcomes for console and test suite

---
+++ lib/input_output.rb
@@ -0,0 +1,18 @@
+class IO
+
+  def self.in
+    print "Please input an option. Choose 'cheap' or 'fast': "
+    gets.chomp
+  end
+
+  def self.out(messages)
+    messages.each { |message| puts message }
+  end
+end
+
+class MockIO
+  # Figure out what the 'in' is
+  def self.out(messages)
+    messages
+  end
+end

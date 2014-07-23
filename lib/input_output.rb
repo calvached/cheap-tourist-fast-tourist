e8a1d00c15e1c50d10e64b8a08f6a98dcc0754a2
calvached <calvache.diana@gmail.com>
2014-07-23 12:13:32 -0500

    Making changes for stdin and stdout

--- lib/input_output.rb
+++ lib/input_output.rb
@@ -1,16 +1,22 @@
 class IO
-  def self.in
-    print "Please input an option. Choose 'cheap' or 'fast': "
-    gets.chomp
+  attr_reader :input, :output
+
+  def initialize(input = $stdin, output = $stdout)
+    @input = input
+    @output = output
   end
 
-  def self.out(messages)
-    messages.each { |message| puts message }
+  def in
+    print "Please input an option. Choose 'cheap' or 'fast': "
+    @input.gets.chomp
+  end
+
+  def out(messages)
+    messages.each { |message| @output.puts message }
   end
 end
 
 class MockIO
-  # Figure out what the 'in' is
   def self.out(messages)
     messages
   end
